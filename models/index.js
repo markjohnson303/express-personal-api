425899c572750bbba05d287060fefcb50e684009
Mark Johnson <markjohnson303@gmail.com>
2016-07-10 17:37:28 -0600

    fix moongoose.connect syntax

--- models/index.js
+++ models/index.js
@@ -1,4 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/personal-api”);
+mongoose.connect( 	process.env.MONGODB_URI ||
+					process.env.MONGOLAB_URI ||
+                  	process.env.MONGOHQ_URL ||
+                  	"mongodb://localhost/personal-api");
 
 module.exports.Campsite = require("./campsite.js");
